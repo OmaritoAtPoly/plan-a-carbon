1a67d4ae30267b55799c8c74179c32c328a72d4f
Omar Estrada Diaz <omarito4857@gmail.com>
2024-11-16 00:56:14 +0200

    adding eslint config

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,11 +1,11 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('App renders', () => {
+test("App renders", () => {
   render(<App />);
 
-  const image = screen.getByRole('img', {
-    name: 'Plan A'
+  const image = screen.getByRole("img", {
+    name: "Plan A"
   });
 
   expect(image).toBeInTheDocument();
